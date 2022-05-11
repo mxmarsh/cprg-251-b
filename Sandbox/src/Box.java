793db7c06a6505a22593af965a3434f8e7c53d94
Dana <63482575+mxmarsh@users.noreply.github.com>
2022-05-11 12:13:08 -0600

    Initalized

---
+++ Sandbox/src/Box.java
@@ -0,0 +1,15 @@
+
+public class Box {
+	
+	private int height;
+	private int depth;
+	private int WIDTH;
+
+	public Box(int height, int depth, int width) {
+		System.out.println("creating a box....");
+	}
+
+	public Box() {
+		
+	}
+}
---
+++ Sandbox/src/Sandbox.java
@@ -0,0 +1,12 @@
+
+public class Sandbox {
+
+	public static void main(String[] args) {
+
+
+		Box box1 = new Box(1, 2, 3);
+
+		System.out.println("end!");
+	}
+
+}
