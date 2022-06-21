0915986149acb9a577e45145cf78a85b1e1e9e1a
Dana <63482575+mxmarsh@users.noreply.github.com>
2022-06-21 14:35:18 -0600

    RAF example

--- Sandbox/src/mod8/StudentExercise.java
+++ Sandbox/src/mod8/StudentExercise.java
@@ -3,11 +3,23 @@ package mod8;
 public class StudentExercise {
 
 	public static void main(String[] args) {
-		// write the student records:
-		// name
-		// age,
-		// GPA,
-		// active status
+		// for reference, student data includes:
+		// name: up to 75 chars
+		// age: max age 100
+		// GPA: 0.0 to 4.0
+		// active status: yes or no
+
+		// write Joe Blow, age 23, GPA 2.1, Active
+
+		// write John Smith, age 45, GPA 3.7, Not Active
+
+		// write Jane Doe, age 41, GPA 2.9, Active
+
+		// write Tom Cruise, age 71, GPA 1.5, not Active
+
+		// then, read back everyone
+
+		// display only active students
 
 	}
 
