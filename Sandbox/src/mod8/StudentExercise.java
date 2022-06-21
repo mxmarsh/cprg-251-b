5c246eff071ecebf292ba1766a5e48056ed75079
Dana <63482575+mxmarsh@users.noreply.github.com>
2022-06-21 14:35:08 -0600

    RAF example

---
+++ Sandbox/src/mod8/StudentExercise.java
@@ -0,0 +1,14 @@
+package mod8;
+
+public class StudentExercise {
+
+	public static void main(String[] args) {
+		// write the student records:
+		// name
+		// age,
+		// GPA,
+		// active status
+
+	}
+
+}
